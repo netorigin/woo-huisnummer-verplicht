d89d3142e6cf488f798189e09f6269e84e1e9175
Netorigin <50382279+netorigin@users.noreply.github.com>
2021-03-11 22:02:08 +0100

    Update woo-huisnummer-verplicht.php

--- woo-huisnummer-verplicht.php
+++ woo-huisnummer-verplicht.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: WooCommerce huisnummer verplicht
  * Plugin URI: https://www.netorigin.nl/
- * Description: Lorem ipsim
+ * Description: WooCommerce Checkout maak huisnummer verplicht
  * Author: Timothy Morgan
  * Version: 1.0.0
  * Author URI: https://netorigin
